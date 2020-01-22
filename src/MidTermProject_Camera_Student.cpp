6e90856d02557813af87ef8402e4902729e03898
Varun <vmalhotra@irobot.com>
2020-01-21 23:21:59 -0500

    Created a circular buffer for img frames

--- src/MidTermProject_Camera_Student.cpp
+++ src/MidTermProject_Camera_Student.cpp
@@ -17,7 +17,18 @@
 #include "matching2D.hpp"
 
 using namespace std;
+constexpr int BUFFER_SIZE = 2;
 
+/**
+ * @brief Get the index of the circular buffer
+ *
+ * @param imgIndex external index
+ * @return int internal index
+ */
+int get_index(int imgIndex)
+{
+    return imgIndex % BUFFER_SIZE;
+}
 /* MAIN PROGRAM */
 int main(int argc, const char *argv[])
 {
@@ -36,7 +47,7 @@ int main(int argc, const char *argv[])
     int imgFillWidth = 4;  // no. of digits which make up the file index (e.g. img-0001.png)
 
     // misc
-    int dataBufferSize = 2;       // no. of images which are held in memory (ring buffer) at the same time
+    int dataBufferSize = BUFFER_SIZE;       // no. of images which are held in memory (ring buffer) at the same time
     vector<DataFrame> dataBuffer; // list of data frames which are held in memory at the same time
     bool bVis = false;            // visualize results
 
@@ -62,7 +73,14 @@ int main(int argc, const char *argv[])
         // push image into data frame buffer
         DataFrame frame;
         frame.cameraImg = imgGray;
-        dataBuffer.push_back(frame);
+        if (dataBuffer.size() < 2)
+        {
+            dataBuffer.push_back(frame);
+        }
+        else
+        {
+            dataBuffer.at(get_index(imgIndex)) = frame;
+        }
 
         //// EOF STUDENT ASSIGNMENT
         cout << "#1 : LOAD IMAGE INTO BUFFER done" << endl;
@@ -115,7 +133,8 @@ int main(int argc, const char *argv[])
         }
 
         // push keypoints and descriptor for current frame to end of data buffer
-        (dataBuffer.end() - 1)->keypoints = keypoints;
+        dataBuffer.at(get_index(imgIndex)).keypoints = keypoints;
+
         cout << "#2 : DETECT KEYPOINTS done" << endl;
 
         /* EXTRACT KEYPOINT DESCRIPTORS */
@@ -126,11 +145,12 @@ int main(int argc, const char *argv[])
 
         cv::Mat descriptors;
         string descriptorType = "BRISK"; // BRIEF, ORB, FREAK, AKAZE, SIFT
-        descKeypoints((dataBuffer.end() - 1)->keypoints, (dataBuffer.end() - 1)->cameraImg, descriptors, descriptorType);
+        descKeypoints(dataBuffer.at(get_index(imgIndex)).keypoints, dataBuffer.at(get_index(imgIndex)).cameraImg, descriptors, descriptorType);
         //// EOF STUDENT ASSIGNMENT
 
         // push descriptors for current frame to end of data buffer
-        (dataBuffer.end() - 1)->descriptors = descriptors;
+        dataBuffer.at(get_index(imgIndex)).descriptors = descriptors;
+
 
         cout << "#3 : EXTRACT DESCRIPTORS done" << endl;
 
@@ -148,14 +168,14 @@ int main(int argc, const char *argv[])
             //// TASK MP.5 -> add FLANN matching in file matching2D.cpp
             //// TASK MP.6 -> add KNN match selection and perform descriptor distance ratio filtering with t=0.8 in file matching2D.cpp
 
-            matchDescriptors((dataBuffer.end() - 2)->keypoints, (dataBuffer.end() - 1)->keypoints,
-                             (dataBuffer.end() - 2)->descriptors, (dataBuffer.end() - 1)->descriptors,
+            matchDescriptors(dataBuffer.at(get_index(imgIndex - 1)).keypoints, dataBuffer.at(get_index(imgIndex)).keypoints,
+                             dataBuffer.at(get_index(imgIndex - 1)).descriptors, dataBuffer.at(get_index(imgIndex)).descriptors,
                              matches, descriptorType, matcherType, selectorType);
 
             //// EOF STUDENT ASSIGNMENT
 
             // store matches in current data frame
-            (dataBuffer.end() - 1)->kptMatches = matches;
+            dataBuffer.at(get_index(imgIndex)).kptMatches = matches;
 
             cout << "#4 : MATCH KEYPOINT DESCRIPTORS done" << endl;
 
@@ -163,9 +183,9 @@ int main(int argc, const char *argv[])
             bVis = true;
             if (bVis)
             {
-                cv::Mat matchImg = ((dataBuffer.end() - 1)->cameraImg).clone();
-                cv::drawMatches((dataBuffer.end() - 2)->cameraImg, (dataBuffer.end() - 2)->keypoints,
-                                (dataBuffer.end() - 1)->cameraImg, (dataBuffer.end() - 1)->keypoints,
+                cv::Mat matchImg = (dataBuffer.at(get_index(imgIndex)).cameraImg).clone();
+                cv::drawMatches(dataBuffer.at(get_index(imgIndex - 1)).cameraImg, dataBuffer.at(get_index(imgIndex - 1)).keypoints,
+                                dataBuffer.at(get_index(imgIndex)).cameraImg, dataBuffer.at(get_index(imgIndex)).keypoints,
                                 matches, matchImg,
                                 cv::Scalar::all(-1), cv::Scalar::all(-1),
                                 vector<char>(), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
