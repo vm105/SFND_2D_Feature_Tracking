d43cd8d8e9926b955dafc40d988f6e0e39dd661d
Varun <vmalhotra@irobot.com>
2020-01-26 15:56:33 -0500

    selected SHITOMASHI with BRIEF as the best combo

--- src/MidTermProject_Camera_Student.cpp
+++ src/MidTermProject_Camera_Student.cpp
@@ -89,7 +89,7 @@ int main(int argc, const char *argv[])
 
         // extract 2D keypoints from current image
         vector<cv::KeyPoint> keypoints; // create empty feature list for current image
-        string detectorType = "AKAZE";
+        string detectorType = "SHITOMASI";
 
         //// STUDENT ASSIGNMENT
         //// TASK MP.2 -> add the following keypoint detectors in file matching2D.cpp and enable string-based selection based on detectorType
@@ -150,7 +150,7 @@ int main(int argc, const char *argv[])
         //// -> BRIEF, ORB, FREAK, AKAZE, SIFT
 
         cv::Mat descriptors;
-        string descriptorType = "BRISK";// BRIEF, ORB, FREAK, AKAZE, SIFT, BRISK
+        string descriptorType = "BRIEF";// BRIEF, ORB, FREAK, AKAZE, SIFT, BRISK
         descKeypoints(dataBuffer.at(get_index(imgIndex)).keypoints, dataBuffer.at(get_index(imgIndex)).cameraImg, descriptors, descriptorType);
         //// EOF STUDENT ASSIGNMENT
 
